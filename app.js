957ae66dff5162737f17c4a7825ac60c9b48da36
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-07-03 14:56:19 +0100

    Add to git because i completely forgot

---
+++ app.js
@@ -0,0 +1,139 @@
+const showModalButton = document.querySelector("#btn-open-modal");
+const closeModalButton = document.querySelector("#btn-close-modal");
+const addBookButton = document.querySelector("#btn-add-book");
+const modal = document.querySelector("dialog");
+const form = document.querySelector("form");
+const formTitle = document.forms["form"]["title"];
+const formAuthor = document.forms["form"]["author"];
+const formPages = document.forms["form"]["pages"];
+const formPrice = document.forms["form"]["price"];
+const formRead = document.forms["form"]["read"];
+
+const myLibrary = [];
+
+function Book(title, author, pages, price, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.price = price;
+	this.read = read;
+
+	this.info = function () {
+		return `${title} by ${author}, ${pages} pages, bought for ${price}, ${
+			read ? "read" : "not read yet"
+		}.`;
+	};
+}
+
+function addBookToLibrary() {
+	const title = prompt("What's the title of the book?");
+	const author = prompt("Who's the author?");
+	const pages = prompt("How many pages is it?");
+	const price = prompt("How much does it cost?");
+	const read = prompt("Have you read it?");
+
+	const newBook = new Book(title, author, pages, price, read);
+	myLibrary.push(newBook);
+
+	console.log("Added Book: ");
+	console.log(newBook);
+}
+
+function displayBooks() {
+	console.log("Rebuilding table...");
+	const table = document.getElementById("table");
+
+	var rowCount = table.rows.length;
+
+	// Loop through the rows, starting from the second row
+	for (var i = rowCount - 1; i > 0; i--) {
+		table.deleteRow(i);
+	}
+
+	for (let i = 0; i < myLibrary.length; i++) {
+		const book = myLibrary[i];
+		const bookRow = document.createElement("tr");
+
+		const titleCell = document.createElement("td");
+		titleCell.textContent = book.title;
+		bookRow.appendChild(titleCell);
+
+		const authorCell = document.createElement("td");
+		authorCell.textContent = book.author;
+		bookRow.appendChild(authorCell);
+
+		const pagesCell = document.createElement("td");
+		pagesCell.textContent = book.pages;
+		bookRow.appendChild(pagesCell);
+
+		const priceCell = document.createElement("td");
+		priceCell.textContent = book.price;
+		bookRow.appendChild(priceCell);
+
+		const readCell = document.createElement("td");
+		readCell.textContent = book.read ? "✅" : "❌";
+		readCell.addEventListener("click", () => {
+			book.read = !book.read;
+			readCell.textContent = book.read ? "✅" : "❌";
+		});
+		bookRow.appendChild(readCell);
+
+		const deleteButton = document.createElement("button");
+		deleteButton.textContent = "X";
+		deleteButton.addEventListener("click", () => {
+			for (let i = 0; i < myLibrary.length; i++) {
+				if (myLibrary[i].title === book.title) {
+					myLibrary.splice(i, 1);
+				}
+				displayBooks();
+			}
+		});
+
+		const deleteCell = document.createElement("td");
+		deleteCell.appendChild(deleteButton);
+		bookRow.appendChild(deleteCell);
+
+		table.appendChild(bookRow);
+	}
+}
+
+function setupEvents() {
+	showModalButton.addEventListener("click", () => {
+		modal.showModal();
+	});
+
+	closeModalButton.addEventListener("click", () => {
+		modal.close();
+	});
+
+	addBookButton.addEventListener("click", (event) => {
+		if (formTitle.value.length === 0) {
+			event.preventDefault();
+			alert("Book title can't be empty!");
+			return;
+		}
+
+		const book = new Book(
+			formTitle.value,
+			formAuthor.value,
+			formPages.value,
+			formPrice.value,
+			formRead.checked
+		);
+		myLibrary.push(book);
+
+		displayBooks();
+		form.reset();
+	});
+}
+
+function start() {
+	const startingBook = new Book("A BOOK", "AN AUTHOR", 9001, 10255, true);
+	myLibrary.push(startingBook);
+
+	displayBooks();
+	setupEvents();
+	console.log("Setup is done!");
+}
+
+start();
